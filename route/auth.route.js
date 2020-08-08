357103ef7c8e9a482d01ac3cb33c71012b9de19a
krystal <nguyenvanthai305@gmail.com>
2020-08-08 20:08:57 +0700

    h27

--- route/auth.route.js
+++ route/auth.route.js
@@ -5,7 +5,6 @@ const passport = require('passport');
 const passportFB= require('passport-facebook').Strategy;
 var controller1= require('../controller/auth.controller');
 var controller2= require('../controller/create.controller');
-var jwt= require('jsonwebtoken')
 
 var upload = multer({ dest: './public/uploads/' });
 
@@ -13,14 +12,11 @@ router.get('/login', controller1.login);
 router.post('/login', controller1.postLogin);
 
 router.get('/fb', passport.authenticate('facebook', {scope: ['email']}));
-router.get('/fb/cb', passport.authenticate('facebook',
-  (req, res, next) => {
-	let token= jwt.sign({ userId: user._id}, "shhhhh");
-		 res.cookie('token', token, { // store it in an https only cookie
-        	signed: true // set to true if your using https
-    	});
-		res.redirect('/');
-}));
+router.get('/fb/cb', passport.authenticate('facebook',  {
+	failureRedirect: '/auth/login',
+	successRedirect: '/'
+})
+);
 router.get('')
 router.get('/create', controller2.create);
 router.post('/create', 
